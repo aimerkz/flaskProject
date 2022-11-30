c0898f820d83b9e430745eac3c7e401e7029a65c
amerkulov <amerkulov@aorti.ru>
2022-11-30 10:57:48 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
         'Flask-Migrate==4.0.0',
         'Flask-RESTful==0.3.9',
         'Flask-SQLAlchemy==3.0.2',
-        'psycopg2-binary==2.9.5'
+        'flask-swagger-ui==4.11.1',
+        'psycopg2-binary==2.9.5',
+        'python-dotenv==0.21.0'
     ],
 )
